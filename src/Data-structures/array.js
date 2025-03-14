ea2433945d2fa0c042c421370dd583fca3461406
yahya <coding404life@gmail.com>
2025-03-14 04:39:11 +0200

    create Javascript array

--- src/Data-structures/array.js
+++ src/Data-structures/array.js
@@ -8,8 +8,66 @@ string.pop(); // O(1) remove item from the end of array
 
 string.shift(); // O(n) as we still alter all the array elements by removing the first element
 
-console.log(string[0]);
+// implementing Javascript  Arrays => Hint: Javascript array are always Dynamic => means every time items added anto the array we don't have to manually allocate space in memory to it
 
-console.log(string);
+class MyArray {
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
 
-// implementing an array
+  get(index) {
+    return this.data[index];
+  }
+
+  // push
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  // pop
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  // delete
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+      console.log(this.data[i], this.data[i + 1]);
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  /*
+  [5,6,7,8]
+  if index = 1
+  i = 1
+  this.data[i] => 6 = this.data[i + 1] => 2
+
+
+*/
+}
+
+const newArr = new MyArray();
+
+newArr.push("hi");
+newArr.push("Yahya");
+newArr.push("ahmed");
+
+const lastItem = newArr.delete(1);
+
+console.log(lastItem);
+console.log(newArr);
